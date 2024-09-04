e85f5733d950453411d8a1b8ee1bf84628edc63f
anlas <1503958337@qq.com>
2024-09-04 16:34:11 +0800

    Site updated: 2024-09-04 16:34:11

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
-import Glide from '@glidejs/glide'
-new Glide('body').mount()
+document.addEventListener("DOMContentLoaded", function () {
+  let isScrolling;
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', function () {
+    window.clearTimeout(isScrolling);
+
+    // 记录最后的滚动位置
+    lastScrollTop = window.scrollY;
+
+    isScrolling = setTimeout(function () {
+      applyGlobalDamping(lastScrollTop);
+    }, 66);
+  });
+
+  function applyGlobalDamping(scrollTop) {
+    let scrollSpeed = scrollTop;
+    const dampingFactor = 0.95; // 阻尼系数，越小阻尼越强
+
+    function animate() {
+      if (Math.abs(scrollSpeed) < 1) return;
+
+      scrollSpeed *= dampingFactor;
+      window.scrollTo(0, scrollTop - scrollSpeed);
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+  }
+});
